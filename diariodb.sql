b688b1c0c9ebf6c500977d6c14a743d82b2a18bb
pintoema522 <pintoema522@gmail.com>
2023-10-25 18:21:14 -0300

    cambio en db

--- diariodb.sql
+++ diariodb.sql
@@ -45,7 +45,7 @@ CREATE TABLE `news` (
 INSERT INTO `news` (`id_noticia`, `id_usuario`, `titulo`, `copete`, `cuerpo`, `imagen`, `fecha`, `categoria`) VALUES
 (19, 1, 'este es un titulo', 'mi copete', 'sdas sad sa dasd \r\n\r\ndas dasd asd sa d\r\n\r\n\r\n\r\nda sd sa dasd sa', '63590331_605.JPG', '2023-09-26', 'Moda'),
 (20, 1, 'dsadsad', 'ddasdasd', '<p>hola</p><p><b>ddddd</b></p><p>gdfgfdg<b><br></b></p><p><br></p>', 'bizzamatero1.PNG', '2023-09-26', 'Deportes'),
-(21, 1, 'el monte everest xD', 'la cuidad cuidosa', '                    <p>dsa as </p><p> das</p><p>d </p><p>d </p><p>asd <br></p>                ', 'city-4457801_640.JPG', '2023-09-26', 'Negocios'),
+(21, 1, 'el monte everest xD', 'la cuidad cuidosa', '                    <p>dsa as </p><p>das</p><p>d </p><p>d </p><p>asd <br></p>                ', 'city-4457801_640.JPG', '2023-09-26', 'Negocios'),
 (22, 1, 'asdas', 'ddddddddddddddddd ddddddddddddddddddddddddd ddddddddddddddd', '<p>ddddddddddddd<br></p>', 'images.JPG', '2023-09-26', 'Deportes'),
 (23, 2, 'esta es la prueba ', 'si sale queda', 'las mejores noticias por el mejor diario de mentirita', '1698172953-capibaras2-770x499.jpg', '2023-10-24', 'Ciencia'),
 (24, 2, 'ahora si', 'queda todo bien ', 'pasdoasdandkandwuabdqkmwdknas', '1698173069-5-tips-para-crear-un-logo-memorable.jpg', '2023-10-24', 'Negocios'),
